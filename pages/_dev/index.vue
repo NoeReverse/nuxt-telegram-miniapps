be862096db7a443a73fbc54b46cc0b89de31eac7
Alexander_Kuzmin <noerevers@gmail.com>
2024-01-26 16:50:29 +0300

    feat: main developer page

---
+++ pages/_dev/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'webapp',
+})
+</script>
+
+<template>
+  <div>
+    <TgPanel title="Developer Pages">
+      <TgCell title="Theme Colors" icon="ph:paint-brush-broad-duotone" to="/_dev/colors" />
+    </TgPanel>
+  </div>
+</template>
