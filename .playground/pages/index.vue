b3455d6e322ad2505a666e977476ed9cda3fc235
Alexander Kuzmin <noerevers@gmail.com>
2024-11-10 16:30:08 +0300

    test: update playgroud

--- .playground/pages/index.vue
+++ .playground/pages/index.vue
@@ -3,17 +3,19 @@ definePageMeta({
   layout: 'webapp',
 })
 const value = ref(0)
+
+usePlatform('ios')
 </script>
 
 <template>
-  <div>
-    <TgSection content>
-      <TgButton size="lg" to="/_dev" icon="material-symbols:qr-code" color="danger">
+  <TgContent>
+    <TgSection content inset>
+      <TgButton size="lg" to="/_dev" icon="material-symbols:qr-code">
         Go to dev page
       </TgButton>
     </TgSection>
 
-    <TgSection title="Is --- still your number?" content>
+    <TgSection title="Is --- still your number?" content inset>
       <p class="tg-hint text-xs mb-4">
         Keep your number up to date to ensure you can always log into Telegram
       </p>
@@ -27,7 +29,7 @@ const value = ref(0)
       </div>
     </TgSection>
 
-    <TgSection title="Account">
+    <TgSection title="Account" inset>
       <TgCell title="@username" description="Username">
         <template #right>
           <TgButton size="sm">
@@ -42,12 +44,25 @@ const value = ref(0)
       </TgCell>
     </TgSection>
 
-    <TgSection title="Settings">
-      <TgCell title="Chat Settings" icon="solar:chat-round-outline" to="/" />
+    <TgSection title="Settings" inset>
+      <TgCell title="Chat Settings" to="/">
+        <template #prepend>
+          <TgIconBox icon="solar:chat-round-outline" size="sm" :round="false" class="bg-lime-500" />
+        </template>
+      </TgCell>
       <TgCell title="Privacy And Security" icon="heroicons:lock-closed" to="/" />
     </TgSection>
 
-    <TgSection title="This device">
+    <TgSection inset>
+      <TgCell title="Chat Settings" to="/">
+        <template #icon>
+          <TgIconBox icon="solar:chat-round-outline" size="sm" :round="false" class="bg-gradient-to-br from-lime-500 to-green-800" />
+        </template>
+      </TgCell>
+      <TgCell title="Privacy And Security" icon="heroicons:lock-closed" to="/" />
+    </TgSection>
+
+    <TgSection title="This device" inset>
       <TgCell title="Samsung Galaxy S22 Ultra" subtitle="Telegram Android 10.1.3" description="Moscow, Russia • online">
         <template #icon>
           <TgIconBox icon="material-symbols:android" round color="primary" />
@@ -56,10 +71,40 @@ const value = ref(0)
       <TgCell title="Terminate All Other Sessions" icon="ion:hand-left-outline" color="danger" />
     </TgSection>
 
-    <TgSection title="Notifications for chats">
+    <TgSection title="Notifications for chats" inset>
       <TgCell title="Private Chats" description="Off, 73 exceptions" />
       <TgCell title="Groups" description="Off, 73 exceptions" />
       <TgCell title="Groups" description="Off, 73 exceptions" />
     </TgSection>
-  </div>
+
+    <TgSection title="TgSection Title" class="mb-6">
+      <TgCell title="Simple TgCell without icon" />
+      <TgCell title="TgCell with to prop" to="/" />
+      <TgCell title="TgCell with to prop and link color" to="/" color="link" />
+      <TgCell title="TgCell with icon and to prop" icon="heroicons:lock-closed" to="/" icon-color="lime" />
+      <TgCell title="This is title" icon="fluent:alert-badge-16-regular" description="This is description for this beautiful cell" icon-color="sky" />
+      <TgCell
+        title="This is title"
+        subtitle="This is subtitle"
+        description="This is description for this beautiful cell"
+        icon="fluent:add-square-32-regular"
+        icon-color="red"
+      />
+
+      <template #append>
+        This is TgSection append slot. Used for some descriptions
+      </template>
+    </TgSection>
+
+    <TgSection title="Section title">
+      <TgCell title="Cell 1" />
+      <TgCell title="Cell 2" />
+    </TgSection>
+
+    <TgSection title="Section title" content>
+      <p>Some section content</p>
+    </TgSection>
+
+    <div class="p-16" />
+  </TgContent>
 </template>
