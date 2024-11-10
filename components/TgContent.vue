f005fc03f29bf75116cd832acf7d62d46a000fcb
Alexander Kuzmin <noerevers@gmail.com>
2024-11-10 15:06:46 +0300

    refactor: make TgContenn client-only and add padding

--- components/TgContent.vue
+++ components/TgContent.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+const { isApple } = usePlatform()
+</script>
+
 <template>
-  <div class="min-h-screen tg-secondary-bg">
-    <slot />
-  </div>
+  <client-only>
+    <div
+      class="min-h-screen flex flex-col gap-4"
+      :class="isApple ? 'pb-6' : ''"
+      :style="{ backgroundColor: 'var(--tg-theme-secondary-bg-color)' }"
+    >
+      <slot />
+    </div>
+  </client-only>
 </template>
