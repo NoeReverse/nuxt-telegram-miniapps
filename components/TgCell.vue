1102562eb3954314c268ee14f7b200477380b544
Alexander Kuzmin <noerevers@gmail.com>
2024-11-10 15:22:39 +0300

    feat(TgCell): adapt cell for ios and macos

--- components/TgCell.vue
+++ components/TgCell.vue
@@ -3,46 +3,57 @@ import type { RouteLocationRaw } from 'vue-router'
 
 const props = withDefaults(
   defineProps<{
+    /** Route location that can be passed to router.push() and other user-facing APIs */
     to?: RouteLocationRaw
+    /** External link */
     href?: string
     rel?: string
     target?: string
     type?: 'button' | 'submit' | 'reset'
+    /** Main text of the cell */
     title?: string
+    /** Not faded small text, positioned before description */
     subtitle?: string
+    /** Faded small text or hint */
     description?: string
+    /** Name of the icon (look: https://icones.js.org/) */
     icon?: string
+    /** Color of the icon (works only on iOS and MacOS) */
+    iconColor?: string
+    /** Line clamp of the subtitle and description */
     lineClamp?: number
+    /** Determines whether to show the border at the bottom */
     border?: boolean
+    /** Can be default, primary, danger */
     color?:
       | 'default'
+      | 'link'
       | 'danger'
   }>(),
   {
     color: 'default',
+    iconColor: 'var(--tg-theme-hint-color)',
     border: true,
   },
 )
 
-const colorClasses = computed(() => ({
-  default: { icon: 'tg-hint', title: 'tg-text' },
-  primary: { icon: 'tg-link', title: 'tg-link' },
-  danger: { icon: 'tg-danger', title: 'tg-danger' },
+const { isApple } = usePlatform()
+
+const colors = computed(() => ({
+  default: { icon: 'var(--tg-theme-hint-color)', title: 'var(--tg-theme-text-color)' },
+  link: { icon: 'var(--tg-theme-link-color)', title: 'var(--tg-theme-link-color)' },
+  danger: { icon: 'var(--tg-theme-destructive-text-color)', title: 'var(--tg-theme-destructive-text-color)' },
 }[props.color]))
 
-const icon = computed(() => {
-  return props.icon ? { size: '21', name: props.icon, class: [colorClasses.value.icon] } : null
-})
-
 const lineClamp = computed(() => {
-  const lines = props.lineClamp ? String(props.lineClamp) : icon.value ? '1' : 'none'
+  const lines = props.lineClamp ? String(props.lineClamp) : props.icon ? '1' : 'none'
   return `line-clamp-${lines}`
 })
 
 const { component, attributes } = useTgButton(props)
 
 const titleProps = computed(() => {
-  return [colorClasses.value.title, props.subtitle && props.description ? 'font-medium text-sm' : '']
+  return [props.subtitle && props.description ? 'font-medium text-sm' : '']
 })
 
 /**
@@ -74,35 +85,48 @@ const pt = computed(() => {
 
 <template>
   <component :is="component" v-bind="attributes" class="w-full group flex select-none bg-transparent" :class="pt">
-    <div class="min-w-[1.25rem] flex">
+    <div class="min-w-[1rem] flex shrink-0">
       <div
-        v-if="icon || $slots.icon"
+        v-if="icon || $slots.prepend"
         class="w-16 flex justify-center"
-        :class="props.description || props.subtitle ? padding : 'items-center'"
+        :class="description || subtitle ? padding : 'items-center'"
       >
-        <slot name="icon">
-          <Icon v-if="icon" :name="icon.name" :class="icon.class" :size="icon.size" />
+        <slot name="prepend">
+          <TgIconBox
+            :icon="props.icon"
+            :size="isApple ? 'sm' : 'lg'"
+            :color="isApple ? 'white' : colors.icon"
+            :background-color="isApple ? iconColor : 'transparent'"
+            :round="false"
+          />
         </slot>
       </div>
     </div>
 
-    <div class="flex-grow flex items-center" :class="[padding, border]">
+    <div class="flex-grow flex items-center pr-5" :class="[padding, border]">
       <div class="flex flex-col justify-center items-start w-full">
-        <p v-if="props.title" class="line-clamp-1 leading-none overflow-visible" :class="titleProps">
-          {{ props.title }}
+        <p
+          v-if="title"
+          class="line-clamp-1 leading-none overflow-visible"
+          :class="titleProps"
+          :style="{ color: colors.title }"
+        >
+          {{ title }}
         </p>
-        <p v-if="props.subtitle" class="text-xs leading-none pt-2" :class="lineClamp">
-          {{ props.subtitle }}
+        <p v-if="subtitle" class="text-xs leading-none pt-2" :class="lineClamp">
+          {{ subtitle }}
         </p>
-        <p v-if="props.description" class="tg-hint leading-none text-xs" :class="[props.subtitle ? 'pt-1.5' : 'pt-2', lineClamp]">
-          {{ props.description }}
+        <p v-if="description" class="tg-hint leading-none text-xs" :class="[subtitle ? 'pt-1.5' : 'pt-2', lineClamp]">
+          {{ description }}
         </p>
 
         <slot />
       </div>
 
-      <div v-if="$slots.right" class="ms-auto shrink-0 pr-5" :class="props.to ? 'pl-4 border-l tg-border' : ''">
-        <slot name="right" />
+      <div v-if="$slots.right || to" class="ms-auto shrink-0 pl-4" :class="!to ? 'border-l tg-border' : ''">
+        <slot name="right">
+          <Icon v-if="isApple" name="fluent:chevron-right-12-filled" class="tg-hint" />
+        </slot>
       </div>
     </div>
   </component>
